4d72b556457928991773037b192f2d8ef35fc335
kertkompus <kert.kompus@tptlive.ee>
2020-06-16 14:25:23 +0300

    lil fixes

--- details.js
+++ details.js
@@ -6,7 +6,15 @@ function getGame() {
 
     let mommaDiv = document.getElementById("details");
     const query = window.location.search.substring(1);
-    const id = query.charAt(query.length-1);
+    const val = query.split("&");
+    const id = val[0].charAt(val[0].length-1);
+
+    const logged = val[1].split("=")[1];
+
+    if(logged==="in"){
+        const loginContainer = document.getElementById("loginContainer");
+        loginContainer.style.display = "none";
+    }
 
     const imagePath = "./images/game" + id + ".jpg";
     const imageDiv = document.createElement("img");
@@ -17,17 +25,32 @@ function getGame() {
     infoDiv.classList.add("info");
 
     const title = titles[id-1];
-    const titleDiv = document.createElement("h2");
+    const titleDiv = document.createElement("h1");
     titleDiv.innerText = title;
 
     const price = prices[id-1];
-    const priceDiv = document.createElement("h3");
+    const priceDiv = document.createElement("h2");
     priceDiv.innerText = price;
 
     const descriptionDiv = document.createElement("p");
     descriptionDiv.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non odio ac elit dictum lobortis ac ac lacus. Vestibulum scelerisque ipsum quis metus lobortis sagittis. Phasellus ac felis sit amet odio scelerisque dictum. Maecenas eget erat placerat orci posuere commodo. Nulla accumsan pulvinar sem, eu sagittis ligula laoreet sit amet. Maecenas feugiat neque eu orci lacinia, vitae lacinia mauris luctus.";
 
     infoDiv.appendChild(titleDiv);
+
+    if(id==="1"){
+        const specialDiv = document.createElement("h2");
+        specialDiv.innerHTML = '<span class="special">32.49€</span> <span class="redText">-20%</span>';
+        priceDiv.classList.add("redText");
+        infoDiv.appendChild(specialDiv);
+    }
+
+    if(id==="3"){
+        const specialDiv = document.createElement("h2");
+        specialDiv.innerHTML = '<span class="special">31.99€</span> <span class="redText">-20%</span>';
+        priceDiv.classList.add("redText");
+        infoDiv.appendChild(specialDiv);
+    }
+
     infoDiv.appendChild(priceDiv);
     infoDiv.appendChild(descriptionDiv);
     mommaDiv.appendChild(imageDiv);
--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-var slideIndex = 0;
+function loginFn() {
+    const loginContainer = document.getElementById("loginContainer");
+    loginContainer.style.display = "none";
+
+    const galleryContainer = document.getElementById("gallery");
+    galleryContainer.innerHTML = '<a href="details.html?id=1&logged=in"><img class="games" src="./images/game1.jpg" alt="Game cover"></a>\n' +
+        '        <a href="details.html?id=2&logged=in"><img class="games" src="./images/game2.jpg" alt="Game cover"></a>\n' +
+        '        <a href="details.html?id=3&logged=in"><img class="games" src="./images/game3.jpg" alt="Game cover"></a>\n' +
+        '        <a href="details.html?id=4&logged=in"><img class="games" src="./images/game4.jpg" alt="Game cover"></a>\n' +
+        '        <a href="details.html?id=5&logged=in"><img class="games" src="./images/game5.jpg" alt="Game cover"></a>\n' +
+        '        <a href="details.html?id=6&logged=in"><img class="games" src="./images/game6.jpg" alt="Game cover"></a>\n' +
+        '        <a href="details.html?id=7&logged=in"><img class="games" src="./images/game7.jpg" alt="Game cover"></a>\n' +
+        '        <a href="details.html?id=8&logged=in"><img class="games" src="./images/game8.jpg" alt="Game cover"></a>';
+}
+
+function logoutFn() {
+    alert("logout");
+    const logoutContainer = document.getElementById("logoutContainer");
+    const loginContainer = document.getElementById("loginContainer");
+    logoutContainer.style.display = "none";
+    loginContainer.style.display = "block";
+}
+
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("bannerSlide");
+
+    let i;
+    const slides = document.getElementsByClassName("bannerSlide");
     //alert(slides.length);
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
@@ -13,10 +37,3 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
-
-function detailPage(index) {
-    var val=index;
-    var queryString = "?game=" + val;
-    window.location.href = "details.html" + queryString;
-}
-
